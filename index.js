d2b420d23ce8e2c91f006aca712344c021b518da
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-11 21:39:56 +0600

    update data

--- index.js
+++ index.js
@@ -67,6 +67,19 @@ async function run() {
             res.send(result)
         })
 
+        app.patch('/bookings/:id',async(req, res)=>{
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const updateBooking = req.body;
+            const updateDoc = {
+                $set: {
+                    status: updateBooking.status
+                },
+              };
+            const result = await bookingCollection.updateOne(filter , updateDoc)
+            res.send(result)
+        })
+
         app.delete('/bookings/:id', async(req , res)=>{
             const id = req.params.id;
             const query = { _id: new ObjectId(id)}
